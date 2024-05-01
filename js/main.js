23e93f0a99b6aaee51ba8fd685a76bd6aceca703
Esther-Omono <enitanatakere@gmail.com>
2024-05-01 15:15:38 +0100

    Added the remaining markup for the site and styled using CSS

--- js/main.js
+++ js/main.js
@@ -3,6 +3,12 @@ const hamburger = document.querySelector(".hamburger");
 const header = document.querySelector("header");
 const logo = document.querySelector(".logo");
 
+const sliderContainer = document.getElementById('sliderContainer');
+let currentGroup = 0;
+const totalGroups = document.querySelectorAll('.slider__group').length;
+
+const currentYear = new Date().getFullYear();
+
 // Header Toggle Menu
 hamburger.addEventListener("click", () => {
     nav.classList.toggle("nav--open");
@@ -23,3 +29,39 @@ window.addEventListener("scroll", () => {
 
 // Creating a Mixer
 var mixer = mixitup(".mixitcontainer");
+
+// Slider
+function goToGroup(index) {
+    const translateValue = `translateX(-${index * (100 / totalGroups)}%)`;
+    sliderContainer.style.transform = translateValue;
+    currentGroup = index;
+}
+
+// Automatically change slide every 3 seconds
+function autoSlide() {
+    currentGroup = (currentGroup + 1) % totalGroups;
+    goToGroup(currentGroup);
+}
+setInterval(autoSlide, 3000);
+
+// Footer - Copyright section
+document.getElementById('currentYear').textContent = currentYear;
+
+// Go to Top
+window.addEventListener('scroll', function() {
+    var scrolled = window.scrollY || document.documentElement.scrollTop;
+    var goTopButton = document.getElementById('goTop');
+
+    if (scrolled > 300) {
+      goTopButton.style.display = 'block';
+    } else {
+      goTopButton.style.display = 'none';
+    }
+});
+
+document.getElementById('goTop').addEventListener('click', function() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+});
